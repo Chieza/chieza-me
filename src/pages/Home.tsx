11413982afb84ee4b810af4c16f230aa2a04c075
Bruno Caliman <brunochiezac@gmail.com>
2025-05-30 19:03:24 -0300

    Scroll Indicator implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import useDocumentTitle from '../hooks/useDocumentTitle'
-import HeroSection from '../components/sections/HeroSection';
-import MeSection from '../components/sections/MeSection';
-import HighlightSection from '../components/sections/HighlightSection';
-import SkillsTools from '../components/sections/SkillsTools';
+import HeroSection from '../components/sections/HeroSection'
+import MeSection from '../components/sections/MeSection'
+import HighlightSection from '../components/sections/HighlightSection'
+import SkillsTools from '../components/sections/SkillsTools'
 import Footer from '../components/sections/Footer'
+import ScrollIndicator from '../components/navigation/ScrollIndicator'
 
 interface HomeProps {
   openContact: () => void
@@ -15,6 +16,7 @@ export default function Home({ openContact }: HomeProps) {
     <>
     {/* Page Sections */}
     <HeroSection onContactClick={openContact}/>
+    <ScrollIndicator />
     <MeSection />
     <HighlightSection />
     <SkillsTools />
