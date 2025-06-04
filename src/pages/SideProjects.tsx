7b2941c2c19226014b77a01871f789f701928f48
Bruno Caliman <brunochiezac@gmail.com>
2025-06-04 18:03:38 -0300

    Placeholder

--- src/pages/SideProjects.tsx
+++ src/pages/SideProjects.tsx
@@ -1,15 +1,22 @@
-import useDocumentTitle from '../hooks/useDocumentTitle'
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import { SectionContentLayout } from "../components/layouts/index";
+import { Link } from 'react-router-dom'
 
 export default function SideProjects() {
-  useDocumentTitle('Side Projects • Bruno')
+  useDocumentTitle("Side Projects • Bruno");
   return (
     <>
-      <div className="text-white p-8 w-full max-h-[1200]">
-        I currently have 2 strong Side Projects, a Whatsapp Bot and this
-        website! I'll publish it ASAP, keep posted! After that I'll keep running
-        new projects and posting some other if I can find it's
-        materials.
-      </div>
+      <SectionContentLayout>
+        <div className="text-white p-8 w-full max-h-[1200]">
+          I currently have 2 strong Side Projects, a Whatsapp Bot and this
+          website! I'll publish it ASAP, keep posted! After that I'll keep
+          running new projects and posting some other if I can find it's
+          materials.
+        </div>
+        <div>
+          <Link to="https://github.com/Chieza/botips" target="_blank">WhatsappBot Link: https://github.com/Chieza/botips</Link>
+        </div>
+      </SectionContentLayout>
     </>
   );
 }
